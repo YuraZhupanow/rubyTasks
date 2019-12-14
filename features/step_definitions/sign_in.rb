6b6c0db3e1fa4e30e8e666efac8d38ea313e4874
Yura Z <yurii@domain.com>
2019-12-14 20:51:45 +0200

    deleted comments

--- features/step_definitions/sign_in.rb
+++ features/step_definitions/sign_in.rb
@@ -1,13 +1,10 @@
 Given(/^I am not logged in$/) do
   @home_page = Home.new
   @home_page.load
-  # visit 'http://192.168.31.110/'
 end
 
 When(/^I click on sign in button$/) do
   @home_page.top_menu.sign_in_link.click
-  #@home_page.sign_in_link.click
-  #find(:xpath, '//*[@id="account"]/ul/li[1]/a').click
 end
 
 
@@ -15,22 +12,16 @@ Then(/^I see a login form opened$/) do
   @log_in = LoginPage.new
   expect(@log_in).to have_username_field
   expect(@log_in).to have_password_field
-  #expect(current_url).to include '/login'
-  #expect(page).to have_content 'Login'
-  #expect(page).to have_content 'Password'
 end
 
 
 When(/^I fill in login form with valid credentials$/) do
   @log_in.username_field.set 'user'
   @log_in.password_field.set 'gvdQWBf2IFYM'
-  #find('#username_field').set 'user'
-  #find('#password_field').set 'gvdQWBf2IFYM'
 end
 
 And(/^Click log in button$/) do
   @log_in.login_submit_button.click
-  #find('#login-submit').click
 end
 
 Then(/^I become a logged in user$/) do
--- page_object/pages/log_in.rb
+++ page_object/pages/log_in.rb
@@ -2,5 +2,4 @@ class Home < SitePrism::Page
   set_url 'http://192.168.31.110/'
 
   section :top_menu, TopSection, '#top-menu'
-    #element :sign_in_link, :xpath, '//*[@id="account"]/ul/li[1]/a'
 end
